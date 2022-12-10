9ee22b25debad7e3803a37b8b54dd93d0b87b4c8
Ümit Tunç <umitreva@gmail.com>
2022-12-10 10:17:07 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+include("vendor/autoload.php");
+// Replace with your own API key
+$apiKey = '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';
+
+// Function to generate a response from GPT-3
+function generateResponse($prompt) {
+    global $apiKey;
+  // Use the completions endpoint to generate a response
+  $response = \Httpful\Request::post('https://api.openai.com/v1/completions')
+    ->body(json_encode(array(
+        'model' => 'text-davinci-003', // Use the Davinci model
+        'prompt' => $prompt,
+        'max_tokens' => 150,
+        'temperature' => 0.9,
+        'top_p' => 1,
+        'n' => 1,
+        'presence_penalty' => 0.6,
+        'frequency_penalty' => 0,
+        'stream' => false,
+        'logprobs' => null,
+        'stop' => '\n'
+      )))
+    ->addHeader('Content-Type', 'application/json')
+    ->addHeader('Authorization', 'Bearer ' . $apiKey)
+    ->send();
+    print_r($response);
+  return $response->body->choices[0]->text;
+}
+
+// Function to handle user input and generate a response
+function handleInput($input) {
+  // Generate a response from GPT-3
+  $response = generateResponse($input);
+
+  // Log the response to the console
+  echo $response . "\n";
+}
+
+handleInput($_GET['text']);
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nategood/httpful' => array(
+            'pretty_version' => '0.3.2',
+            'version' => '0.3.2.0',
+            'reference' => '0cded3ea97ba905600de9ceb9ef13f3ab681587c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nategood/httpful',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
